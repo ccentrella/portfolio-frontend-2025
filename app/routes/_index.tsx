fd0c321d17d5e81db127101615973fe70568e185
Christopher Centrella <ccentrella.j@gmail.com>
2024-08-06 19:22:25 -0500

    configure eslint

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -4,7 +4,7 @@ function Index(props) {
     return (
         <div>
             <h1>Welcome!</h1>
-            <p>Chris Centrella's Portfolio 2025</p>
+            <p>Chris Centrella&lsquo;s Portfolio 2025</p>
         </div>
     );
 }
