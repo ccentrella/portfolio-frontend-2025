462ccc0dff13ab3a9e6e229ef899be3c40ca7308
Christopher Centrella <ccentrella.j@gmail.com>
2024-08-06 22:13:10 -0500

    update Hero component

--- app/components/Hero/Hero.tsx
+++ app/components/Hero/Hero.tsx
@@ -1,32 +1,33 @@
 import React from 'react';
+import styles from './Hero.module.scss';
 
 function Hero(props) {
     return (
         <>
-            <div className='container'>
-                <div className='left-panel'>
-                    <div className='profile-pic'>
+            <div className={styles.container}>
+                <div className={styles.leftPanel}>
+                    <div className={styles.profilePic}>
                         <img src='public/profile.png' alt='profile picture' />
                     </div>
-                    <div className='text-content'>
+                    <div className={styles.textContent}>
                         <p>&lsquo;Sup? My name is </p>
-                        <p className='name'>Chris Centrella.</p>
+                        <p className={styles.name}>Chris Centrella.</p>
                     </div>
                 </div>
-                <div className='right-panel'>
-                    <div className='bg-box'></div>
+                <div className={styles.rightPanel}>
+                    <div className={styles.bgBox}></div>
                 </div>
             </div>
             <div>
-                <h2 className='section-heading'>Industry Experience.</h2>
+                <h2 className={styles.sectionHeading}>Industry Experience.</h2>
                 <div></div>
             </div>
             <div>
-                <h2 className='section-heading'>Technical Experience.</h2>
+                <h2 className={styles.sectionHeading}>Technical Experience.</h2>
                 <div></div>
             </div>
             <div>
-                <h2 className='section-heading'>Causes.</h2>
+                <h2 className={styles.sectionHeading}>Causes.</h2>
                 <div></div>
             </div>
         </>
