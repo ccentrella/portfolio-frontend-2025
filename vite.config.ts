406e92d866fedb6d53db2f5e3cba74769f55fa5c
Christopher Centrella <ccentrella.j@gmail.com>
2024-08-06 21:49:06 -0500

    add tsconfigpaths plugin, remove express

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,23 @@
 import { vitePlugin as remix } from '@remix-run/dev';
 import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  cacheDir: '.cache',
-  server: {
-    port: 3000,
-  },
-  plugins: [
-    remix({
-      buildDirectory: '.build',
-      ignoredRouteFiles: ['**/.*'],
-      ssr: false,
-    }),
-  ],
-  css: {
-    modules: {
-      localsConvention: 'camelCase',
+    cacheDir: '.cache',
+    server: {
+        port: 3000,
     },
-  },
+    plugins: [
+        remix({
+            buildDirectory: '.build',
+            ignoredRouteFiles: ['**/.*'],
+            ssr: false,
+        }),
+        tsconfigPaths(),
+    ],
+    css: {
+        modules: {
+            localsConvention: 'camelCase',
+        },
+    },
 });
